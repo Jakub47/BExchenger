72d418a47903df322641dd479981851c6e4cbf3f
Jakub47 <jakub7447@gmail.com>
2019-01-06 01:08:14 +0100

    Dodanie contextu & utworzenie bazy

--- Wymieniator/Wymieniator/Controllers/HomeController.cs
+++ Wymieniator/Wymieniator/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wymieniator.DAL;
+using Wymieniator.Models;
 
 namespace Wymieniator.Controllers
 {
     public class HomeController : Controller
     {
+        private WymieniatorContext db = new WymieniatorContext();
+        
         // GET: Home
         public ActionResult Index()
         {
+            var category = new Category
+            {
+                Name = "Fantasy",
+                Description = "Kategoria dla książek fantastycznych",
+                Picture = "Fantasy.png"
+            };
+            db.Categories.Add(category);
+            db.SaveChanges();
             return View();
         }
     }
---
+++ Wymieniator/Wymieniator/DAL/WymieniatorContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Wymieniator.Models;
+
+namespace Wymieniator.DAL
+{
+    public class WymieniatorContext : DbContext
+    {
+        public WymieniatorContext() : base("WymieniatorContext")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Exchange> Exchanges { get; set; }
+        public DbSet<PositionOfExchange> PositionOfExchanges { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
