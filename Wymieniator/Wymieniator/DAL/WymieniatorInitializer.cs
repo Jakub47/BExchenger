fdd37ca15af3b009cb89854920e28442ae836928
Jakub47 <jakub7447@gmail.com>
2019-01-08 23:23:08 +0100

    Initializer 1/2

---
+++ Wymieniator/Wymieniator/DAL/WymieniatorInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Wymieniator.DAL
+{
+    public class WymieniatorInitializer : DropCreateDatabaseAlways<WymieniatorContext>
+    {
+        protected override void Seed(WymieniatorContext context)
+        {
+            SeedData(context);
+            base.Seed(context);
+        }
+
+        private void SeedData(WymieniatorContext context)
+        {
+            //Create and add objects to db! 
+        }
+    }
+}
