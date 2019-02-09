6e282cc9c33f46d60998c55ac551e9de89731eb7
Jakub47 <jakub7447@gmail.com>
2019-02-09 15:58:06 +0100

    Bundle

---
+++ Wymieniator/Wymieniator/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Wymieniator.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jqueryAndjqueryUIAndFilesForWebsite").Include(
+                                        "~/Scripts/jquery-{version}.js",
+                                        "~/Scripts/jquery-ui-{version}.js",
+                                        "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                                        "~/Content/vendor/jquery-easing/jquery.easing.min.js",
+                                        "~/Scripts/js/sb-admin.min.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                                        "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                                        "~/Content/vendor/fontawesome-free/css/all.min.css",
+                                        "~/Content/vendor/datatables/dataTables.bootstrap4.css",
+                                        "~/Content/css/sb-admin.css"));
+        }
+    }
+}
