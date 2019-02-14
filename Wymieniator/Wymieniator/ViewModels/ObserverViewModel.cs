aece12a57c66a4e833274a98c77635fc028b6370
Jakub47 <jakub7447@gmail.com>
2019-02-14 23:55:53 +0100

    Added view for observre

---
+++ Wymieniator/Wymieniator/ViewModels/ObserverViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wymieniator.Models;
+
+namespace Wymieniator.ViewModels
+{
+    public class ObserverViewModel
+    {
+        public List<ObservePosition> ObservePosition { get; set; }
+    }
+}
