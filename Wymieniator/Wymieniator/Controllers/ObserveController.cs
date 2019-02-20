ea10c7f20843f45aed6564af02fd474311d28891
Jakub47 <jakub7447@gmail.com>
2019-02-20 23:14:53 +0100

    Views for Observer must be improved but logic works

--- Wymieniator/Wymieniator/Controllers/ObserveController.cs
+++ Wymieniator/Wymieniator/Controllers/ObserveController.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wymieniator.DAL;
+using Wymieniator.Infrastucture;
+using Wymieniator.ViewModels;
 
 namespace Wymieniator.Controllers
 {
     public class ObserveController : Controller
     {
+        private ObserveManager observeManager;
+        private ISessionManager sessionManager { get; set; }
+        private WymieniatorContext db = new WymieniatorContext();
+
+        public ObserveController()
+        {
+            sessionManager = new SessionManager();
+            observeManager = new ObserveManager(sessionManager, db);
+        }
+
         // GET: Observe
         public ActionResult Index()
         {
-            return View();
+            var elementsObserver = observeManager.GetObserver();
+            ObserverViewModel vm = new ObserverViewModel()
+            {
+                ObservePosition = elementsObserver
+            };
+            return View(vm);
         }
 
         public ActionResult AddToObserve(int id)
         {
-            return View();
+            observeManager.AddToObserver(id);
+
+            return RedirectToAction("Index");
         }
     }
 }
