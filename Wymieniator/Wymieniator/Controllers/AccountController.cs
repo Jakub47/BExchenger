02e7055249f653ec9c85c752eebdde71d37cb368
Jakub47 <jakub7447@gmail.com>
2019-02-25 22:45:33 +0100

    Logowanie + Rejestracja Compelted & Dodane skrypty do filtrowania danych po stronie front-ednu

--- Wymieniator/Wymieniator/Controllers/AccountController.cs
+++ Wymieniator/Wymieniator/Controllers/AccountController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wymieniator.ViewModels;
 
 namespace Wymieniator.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Login()
+        public ActionResult Login(string ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string ReturnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+
+        }
+
+        public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+
         }
     }
 }
